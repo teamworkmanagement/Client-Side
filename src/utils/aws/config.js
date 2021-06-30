5bb7374e22bb7b54db9c7d60b12ff8a9d01fe65f
Dung Nguyen <dunghkuit@gmai.com>
2021-06-30 22:57:43 +0700

    change aws key

--- src/utils/aws/config.js
+++ src/utils/aws/config.js
@@ -4,8 +4,8 @@ const REGION = 'ap-southeast-1';
 
 
 AWS.config.update({
-    accessKeyId: 'AKIAYN6LHKCURUYRT6W3',
-    secretAccessKey: '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'
+    accessKeyId: 'AKIAYN6LHKCU4EYT2EMT',
+    secretAccessKey: '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'
 })
 
 export const myBucket = new AWS.S3({
